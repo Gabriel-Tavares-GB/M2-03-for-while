fcacd020a78831915e9ad241a649d34968b6532d
Gabriel-Tavares-GB <gbtav83@gmail.com>
2021-05-21 21:19:16 -0300

    Frist commit, Desafio for-while

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+1 - Criar um loop que conte de 1 até 10 usando FOR
+
+
+2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+
+3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+
+
+4- Criar um loop que conte todos os números pares de 0 a 100
+*/
+
+
+/*
+for (let x = 1; x <= 10; x += 1)
+    console.log(x)
+*/
+
+
+/*
+var cont = 10;
+while (cont >= 1) {
+    document.write(cont);
+    cont--;
+}
+// Resultado na página!*/
+
+
+/*
+for (let x = 1; x <= 100; x += 2)
+    console.log(x)
+    */
+
+
+
+/*
+for (let x = 0; x <= 100; x += 2)
+    console.log(x)
+    */
